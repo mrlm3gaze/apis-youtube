c282b80a4b6cde9fa1dcd3a95b1454c90cc26f12
mrlm3gaze <47802306+mrlm3gaze@users.noreply.github.com>
2020-03-04 23:57:52 +0100

    Create items V7.js

---
+++ items V7.js
@@ -0,0 +1,377 @@
+let ul = document.getElementById("index_tub"),
+    li = ul.getElementsByTagName("li"),
+		vscroll=document.querySelector('#top'),
+		switchMode=document.querySelector('.switchM'),
+		innerNightM=document.querySelector('.NightM'),
+		input = document.getElementById("myInput"),
+    nightMode=0,
+		select =document.querySelector(".slection"),
+		optCotry=select.getElementsByTagName('option'),
+		lang=document.getElementById('lang'),
+		opt=lang.getElementsByTagName('option'),
+		search=document.querySelector(".search"),
+		Name_Country=document.querySelector(".name_cuntry_ip"),
+		country ='',
+		listItem=[],
+		ListFav=[],
+		pos='beforeend',
+		text,
+		// vars lang
+	    lg=localStorage.getItem('!Lan?gua_ge'),
+	    lgWv='',
+	    lgDsW='',
+	    lgNds='',
+	    tren=document.querySelector('.title'),
+	    lgWlg=document.getElementById('word_lang'),
+	    StyleItem=document.createElement('style');
+// add new style and select language 
+StyleItem.setAttribute('type','text/css')
+	    if (lg==null || lg==undefined || lg=='' || lg=='?:e' || lg!='?:e'&& lg!='?:a') {
+	     		opt[0].setAttribute("selected","0");
+	     		opt[0].textContent='English';
+	     		opt[1].textContent=' العربية  ';
+	     		input.setAttribute('placeholder','Search');
+	     		lgWlg.textContent='Language : ';
+	     		tren.textContent='Tranding in : ';
+	     		lgWv='Whatch Video';
+	     		lgDsW='Description';
+	     		lgNds='No Description ☻';
+	     		// new style
+	     		StyleItem.innerHTML=`
+	     			/*=======top=======*/
+	     			nav.rightElemnt{float:left}
+	     			nav.leftElemnt{float:right}
+	     			.title,.slection,.language,.word_lang {float: left;}
+	     			#lang{direction: ltr;}
+	     			.other_btn{float:right}
+	     		    button.switchM.btn {float: left;}
+	     			button.Close.btn {float: right;}
+	     			/*====add by js======*/ 
+	     			.img_i:hover .pub {right: 2px;}
+	     			.pub{right:-100px}
+	     			/*****************************/
+	     			@media only screen and (max-width: 1020px) {
+					nav.leftElemnt{width: 100%;float: none;text-align: center;}
+					nav.rightElemnt{width: 100%;float: none;text-align: center;}
+					.other_btn{float: none;display: inherit;margin: 0}
+					.search {width: 90%;}
+				  }
+	     		`
+	    } else if(lg=='?:a'){
+	     		opt[1].setAttribute("selected","1");
+	     		opt[0].textContent=' English';
+	     		opt[1].textContent='العربية ';
+	     		input.setAttribute('placeholder','بحث ');
+	     		lgWlg.innerHTML='اللغة  : ';
+	     		tren.textContent='المحتوى الرائج في : ';
+	     		lgWv='مشاهدة الفيديو ';
+	     		lgDsW=' الوصف ';
+	     		lgNds='لايوجد وصف للفيديو  ☻';
+
+	     		// new style
+	     		StyleItem.innerHTML=`
+	     			/*=======top=======*/
+	     			nav.rightElemnt{float:right}
+	     			nav.leftElemnt{float:left}
+	     			.title,.slection,.language,.word_lang {float: right;}
+	     			#lang{direction: rtl;}
+	     			.other_btn{float:left}
+	     			button.switchM.btn {float: right;}
+	     			button.Close.btn {float: left;}
+	     			/*====add by js======*/ 
+	     			.img_i:hover .pub {left: 2px;}
+	     			.pub{left:-100px}
+	     			/*****************************/
+	     			@media only screen and (max-width: 1020px) {
+					nav.leftElemnt{width: 100%;float: none;text-align: center;}
+					nav.rightElemnt{width: 100%;float: none;text-align: center;}
+					.other_btn{float: none;display: inherit;margin: 0}
+					.search {width: 90%;}
+				  }
+	     		`
+	     }
+document.getElementsByTagName("head")[0].appendChild(StyleItem);
+//added events listener 
+document.addEventListener('load',getTube(country));
+//added slect
+select.addEventListener("change", changed);
+// change country
+function changed(country){
+	ul.innerHTML ='';
+	LastPosCount={
+		'_last_Position':select.options[select.selectedIndex].id,
+		'_last_Country':select.value
+	}
+	localStorage.setItem('!lastPosition_&_county',JSON.stringify(LastPosCount))
+	getTube(select.options[select.selectedIndex].value);
+	listItem=[];
+}
+//get tube maroc
+function getTube(country){
+	var lasTpoScounT=JSON.parse(localStorage.getItem('!lastPosition_&_county'));
+
+	if (lasTpoScounT=='' ||lasTpoScounT==null||lasTpoScounT==undefined)
+	{
+		LastPosCount={
+		'_last_Position':0,
+		'_last_Country':'MA'
+		}
+		localStorage.setItem('!lastPosition_&_county',JSON.stringify(LastPosCount))
+		country='MA';
+		optCotry[0].setAttribute('selected','')
+	}
+	else{
+		var place=lasTpoScounT['_last_Position'];
+			cutry=lasTpoScounT['_last_Country'];
+
+		if (cutry==''|| cutry=='MA' || cutry=='undefined' && place==''|| place==0 || place=='undefined' )
+		 {
+				country='MA';
+				optCotry[0].setAttribute('selected','')
+
+		 } else {
+				country=cutry;
+				optCotry[place].setAttribute('selected','')		
+			}
+	}
+	//get Data From Url
+
+	var url=`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostpopular&regionCode=${country}&maxResults=50&key=${apikey}`,
+		itemStaticUrl=`https://www.googleapis.com/youtube/v3/videos?part=statistics&chart=mostpopular&regionCode=${country}&maxResults=50&key=${apikey}`,
+	    req = new Request(url),
+	    results,
+	    articls,
+	    UrlYoutube;
+	fetch(req)
+	    .then(function(response) {
+	    	results =response.json();
+	     	results.then( result => {
+	     		articls=result.items;
+	    		for (var i = 0; i < articls.length ; i++) {
+	    				// create element li
+	     			var DIV=document.createElement('li'),
+	     				// date
+	     			    dateSource=articls[i].snippet.publishedAt,
+	     			    date  = dateSource.slice(0, 10) +" | " + dateSource.slice(11,16) ,
+	     			    // title video
+	     			    title = articls[i].snippet.title,
+	     			    subTitle='',
+	     				title1 = title.replace(/['"]/gi, 'mrlm3gaze'),
+	     				title2=encodeURIComponent(title1),
+	     				// title channel
+	     				Ctle  = (articls[i].snippet.channelTitle).slice(0,30),
+	     				NmChOr=articls[i].snippet.channelTitle.replace(/["']/gi,'mrlm3gaze'),
+	     				NmCh  =encodeURIComponent(NmChOr),
+						//  link channel
+						ChaLin=articls[i].snippet.channelId.replace(/["']/gi,'mrlm3gaze'),
+						linCod=encodeURIComponent(ChaLin),
+	     				// tags
+						tagsOr= articls[i].snippet.tags,
+						tags=encodeURIComponent(tagsOr).replace(/["']/gi,'mrlm3gaze'),
+	     				// description
+	     				desc  = '',
+	     				desc2='',
+	     				// thumbnails video
+	     				imageUrl='';
+					//if else
+					// title
+	     			if (articls[i].snippet.title.length > 60 ) {
+	     					subTitle=title.substring(0, 60) + '...';
+	     			} else {
+	     					subTitle=title;
+	     			}
+	     			//image onerror 
+	     				if (articls[i].snippet.thumbnails.high.url.onerror) {
+	     					imageUrl="https://lh3.googleusercontent.com/-cqHslEVUe_c/XhJ1b0b5XLI/AAAAAAAADEo/hTcTlKdq5jgaH-DRm3fwsPUvZGOoV4S3ACEwYBhgL/h120/image-placeholder.png";
+	     				}else{
+	     					imageUrl=articls[i].snippet.thumbnails.high.url;
+	     				}
+	     			//descreption lenght > 200 char
+	     				if ((articls[i].snippet.description).length > 300) {
+	     						desc= (articls[i].snippet.description).substring(0, 300) + ' ...';
+	     						desc2=encodeURIComponent(articls[i].snippet.description).replace(/['"]/gi,'mrlm3gaze');
+	     				}else{
+	     					    desc= articls[i].snippet.description;
+	     					    var desc2=encodeURIComponent(desc).replace(/['"]/gi,'mrlm3gaze');
+	     					    if (desc==="") {
+	     						 	 desc=lgNds;
+	     						 	 desc2=encodeURIComponent(desc);
+	     						 }else {
+	     						 	desc=articls[i].snippet.description;
+	     						 	desc2=encodeURIComponent(desc).replace(/['"]/gi,'mrlm3gaze');
+	     						 }
+	     				}
+	     				 text=`
+	     				<li class="elments" id="${i}">
+	     					 <div class='favorite' id="favorite">
+	     						<i class="material-icons" id='${i}' job="addToFav">favorite_border</i>
+	     		  			</div>
+	     			   		<div class="num_tend"> ${i+1} </div>
+							<a href="view Video tube.html" title="${title}">
+								<h3 class="title_element" id="${i}">${subTitle}</h3>
+							</a>
+							<div class="Name_Chan">
+								<a href="https://www.youtube.com/channel/${articls[i].snippet.channelId}" target="_blink"><i class="material-icons">account_circle</i><strong class="name_c" title="${Ctle}">${Ctle}</strong>
+								</a>
+							</div>
+							<a href="view Video tube.html" id="${i}" title="${title}">
+								<div class="img_i">
+									  <span class="play" id="${i}">
+									  	 <i class="material-icons" id="${i}">play_circle_outline</i>
+									  </span>
+									  <div style='background:url(${imageUrl}) no-repeat center;background-size:cover' class="img" id="${i}"></div>
+								</a>
+									<span class="pub"> ${date} </span>
+									<span class='views'></span>
+									<!--
+									<div class="desc_p">
+										<p class="desc"> ${lgDsW} </p>
+										<p class="para"> ${desc}</p>
+									</div>
+									-->
+					      		</div>
+							<a class="link" href="view Video tube.html" id="${i}" title="${title}"> ${lgWv} </a>
+						</li>
+	     			`;
+	     			ul.insertAdjacentHTML(pos,text);
+
+      				listItem.push({'url':articls[i].id, 'Title':title2, 'tags':tags, 'date':date, 'desc':desc2, 'nameChannel':NmCh, 'LinkChannel':linCod, 'trend':i+1,'poster':imageUrl});
+	    		}
+	     	})
+	        
+	    });
+}
+// slect Element and add local Storage 
+ul.onclick = function ClickItem(event){
+ 	var Element=event.target,
+ 		Item = Element.getAttribute('id'),
+ 		favorit=Element.getAttribute('job');
+ 		try {
+	 		let url=listItem[Item].url,
+	 			Title=listItem[Item].Title,
+	 			tags=listItem[Item].tags,
+	 			date=listItem[Item].date,
+	 			desc=listItem[Item].desc,
+	 			nameChannel=listItem[Item].nameChannel,
+	 			LinkChannel=listItem[Item].LinkChannel,
+	 			trend=listItem[Item].trend,
+	 			poster=listItem[Item].poster,
+
+	 			obj={url:url, Title:Title, tags:tags, date:date, desc:desc, nameChannel:nameChannel, LinkChannel:LinkChannel, trend:trend, poster:poster};
+
+	 			sessionStorage.setItem('trend_Data_?Vedio',JSON.stringify(obj));
+	 			var dataLocale=JSON.parse(localStorage.getItem('DataForFav'))
+	 		if (favorit=='addToFav') {
+	 			// favorit=Element.setAttribute('style','color:red')
+	 			// favorit=Element.innerHTML='favorite';
+	 			// if (dataLocale.includes(Title)) {
+	 			// 	favorit=Element.setAttribute('style','color:red')
+	 			// 	favorit=Element.innerHTML='favorite';
+	 			// } else {
+	 			// 	favorit=Element.setAttribute('style','')
+	 			// 	favorit=Element.innerHTML='favorite_border';
+	 			// }
+	 			 alert('Coming soon')
+	 		
+	 			 // ListFav.push(obj)
+
+ 				 // localStorage.setItem('DataForFav',JSON.stringify(ListFav))
+ 				 
+ 				}
+ 		} catch(err){
+ 			console.log(err);
+ 			    
+ 				
+ 		}		
+}
+// search
+input.addEventListener('input',function Search() {
+    var  filter, a, txtValue;
+    filter = input.value.toUpperCase();
+    for (var i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName('a')[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "block";
+        } else {
+            li[i].style.display = "none";
+        }
+		
+    }
+});
+// scroll
+$( window ).on('scroll',function() {
+	if ($(window).scrollTop()>=150) {
+		vscroll.style.position = 'fixed';
+	} else {
+		vscroll.style.position = 'relative';
+	}
+})
+// change color 
+switchMode.addEventListener("click", function(){
+	nightMode++
+if (nightMode==1) 
+	{	
+		localStorage.setItem('!Bkg','1')
+		ul.style.background = '#1f1f1f';
+		ul.style.color = '#c5c5c5';
+		vscroll.style.background = '#292929'; 
+		vscroll.style.color = '#fff';
+		innerNightM.innerHTML='brightness_5';
+		return nightMode == 2;
+	} 
+else if (nightMode==2)
+{
+	localStorage.setItem('!Bkg','0')
+	ul.style.background = '#f9f9f9';
+	ul.style.color = '#484242';
+	vscroll.style.background = '#fff'; 
+	vscroll.style.color = '#484242';
+	innerNightM.innerHTML='brightness_4';
+	return nightMode=0;
+}
+});
+// get background
+window.onload = function GetBg() {
+	var bg=localStorage.getItem('!Bkg');
+	if (bg=='' ||bg==undefined||bg==null || bg=='0' ||bg!='1' && bg!='0') 
+	{
+		ul.style.background = '#f9f9f9';
+		ul.style.color = '#484242';
+		vscroll.style.background = '#fff'; 
+		vscroll.style.color = '#484242';
+		innerNightM.innerHTML='brightness_4';
+		nightMode=0;
+
+	} 
+	else if(bg=='1' )
+	{
+		ul.style.background = '#1f1f1f';
+		ul.style.color = '#c5c5c5';
+		vscroll.style.background = '#292929'; 
+		vscroll.style.color = '#fff';
+		innerNightM.innerHTML='brightness_5';
+		nightMode=1;
+	}
+}
+//lang
+lang.addEventListener('change', ChangeLang)
+function ChangeLang(){
+	location.reload();
+	if (lang.value=='en' || lang.value=='') {
+		localStorage.setItem('!Lan?gua_ge',"?:e");
+	} else if(lang.value=='ar'){
+		localStorage.setItem('!Lan?gua_ge',"?:a");
+	}	
+}
+let xhr=new XMLHttpRequest();
+xhr.onload = function(){
+	 if (this.status==200) {
+		var Response=JSON.parse(this.response);
+		Name_Country.textContent=Response.country;
+	 } else {
+	 	console.warn('error for getting Data');
+	 }
+}
+xhr.open('GET','http://ip-api.com/json');
+xhr.send();
